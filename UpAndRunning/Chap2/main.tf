08b04f78e0d794bcb7b83d1c0cd5097128fe7ca1
Marvin Carpenter <marvin.carpenter@gmail.com>
2020-05-31 12:29:11 -0500

    added variables

--- UpAndRunning/Chap2/main.tf
+++ UpAndRunning/Chap2/main.tf
@@ -1,11 +1,38 @@
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "terraform-example"
-    }
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-example"
+  }
+  user_data              = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup busybox httpd -f -p ${var.server_port} &
+    EOF
+  vpc_security_group_ids = [aws_security_group.instance.id]
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The web server port number"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
