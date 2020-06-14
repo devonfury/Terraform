0d8cafd528ececf3079e58ea8817ba8cba02dfab
Marvin Carpenter <marvin.carpenter@gmail.com>
2020-06-14 17:26:33 -0500

    added outputs

--- UpAndRunning/Chap3/main.tf
+++ UpAndRunning/Chap3/main.tf
@@ -38,3 +38,13 @@ terraform {
         encrypt = true
     }
 }
+
+output "s3_bucket_arn" {
+    value = aws_s3_bucket.chapter3_state.arn
+    description = "The ARN of the S3 bukcet"
+}
+
+output "dynamodb_table_name" {
+    value = aws_dynamodb_table.terraform_locks.name
+    description = "The name of the DynamoDB table"
+}
